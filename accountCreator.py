f131145a8655b1685de25bcc9b89356396311de4
IbraTheInfoSecStudent <60770005+IbraTheInfoSecStudent@users.noreply.github.com>
2020-02-10 19:40:36 -0500

    added comments

--- accountCreator.py
+++ accountCreator.py
@@ -2,54 +2,63 @@
 # Description: Account creator program. Users can create accounts and save them to a file.
 # Accounts from files can be retrieved. Save files can be deleted. Account details can be viewed.
 
+# Required modules
 import pickle
 import os
 
 
+# Creating a class Account. Each account the user creates is an object of this class
 class Account:
     def __init__(self, fullname, username, password):
         self.fullname = fullname
-        # username is the unique value
+        # Username is the unique value
         self.username = username
         self.password = password
 
     def getUserInfo(self):
-        print(f"full Name: {self.fullname.title()}\nUsername: {self.username}\nPassword: {self.password}")
+        print(f"Full Name: {self.fullname.title()}\nUsername: {self.username}\nPassword: {self.password}")
 
 
-active = True
-accountDictionary = {}
-
-
+# This function creates accounts. Requires user to input their name, desired username, and desired password
 def createAccount():
     name = input('Enter your full name: ')
     username = input('Enter a username of your choice: ')
-    password = input('Enter a password of your choice: ')
+    password = input('Enter a password of your choice: ')    
+    # Checks if the username the user chose is already chosen. If it is, it forces the user to choose another username
     if username in accountDictionary.keys():
         print('That username is taken. Chose another')
-        createAccount()
+        createAccount()    
+    # If the username is unique, the information is added to a dictionary. The key is the username, and the value
+    # that corresponds to that key is an object of the class Account
     else:
         accountDictionary[username] = Account(name, username, password)
         print('User successfully added!')
 
 
-def displayAccounts():
+# This functions displays existing accounts
+def displayAccounts():    
+    # If there is ever a case where the dictionary is empty, that means there are no existing accounts
     if len(accountDictionary) == 0:
-        print('There are no existing accounts')
+        print('There are no existing accounts')       
+    # Displays usernames of all accounts
     else:
         print('Existing accounts:')
         for keys in accountDictionary.keys():
             print('\t' + keys)
 
 
+# This function displays all data for a chosen account            
 def displayData():
-    print('\nFor which account do you want to see the data?')
+    print('\nFor which account do you want to see the data?')    
+    # Displays each username so the user can pick which account they want to view information for
     for keys in accountDictionary.keys():
         print('\t' + keys)
     user = input('\nEnter account name: ')
-    if user in accountDictionary.keys():
+    if user in accountDictionary.keys():       
+        # Loops through the entire dictionary, but only prints data once it finds the username the user chose
         for key, value in accountDictionary.items():
             if user == key:
+                # Calls the method defined in the class Account
                 print(value.getUserInfo())
             elif user != key:
                 continue
@@ -57,28 +66,39 @@ def displayData():
         print('That user does not exist')
 
 
+# This method will save the current accounts to a file
+# It uses methods from the os and pickle modules
+# Future additions to this function:
+#   1. More input validation to prevent the user from writing to and overwriting a file that is not a save file
 def writeToFile(dictionary):
     print('Current account save files: ')
+    # Puts all file names that exist in the current directory into the files list
     files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    # Prints file names from the files list only if it contains a .txt, as that means it is a save file
     for f in files:
         if '.txt' in f:
             print('\t' + f)
         else:
             continue
-
+            
     filechoice = input('\nWhich file should the accounts be saved to?\nIt can be one of the above files, or a new one.'
                        '\nCAUTION: Existing files will be overwritten.\tChoose file: ')
+    # It does not matter if the user includes the .txt in what they input
     if '.txt' in filechoice:
         filename = filechoice
     else:
         filename = (filechoice + '.txt')
     with open(filename, 'wb') as f:
+        # Uses pickle's dump() function to put all accounts from the dictionary into the save file
+        # If save file does not exist, it creates a new file automatically
         pickle.dump(dictionary, f)
     print('Saved to a file')
 
 
+# This function will restore/retrieve from a save file to continue where you left off
 def retrieveFromFile():
     print('List of account save files:')
+    # Uses same code as above to print off save files
     files = [f for f in os.listdir('.') if os.path.isfile(f)]
     for f in files:
         if '.txt' in f:
@@ -90,8 +110,10 @@ def retrieveFromFile():
         filename = filechoice
     else:
         filename = (filechoice + '.txt')
+    # Try and except block in order to catch the FileNotFoundError
     try:
         with open(filename, 'rb') as f:
+            # Uses pickle's load() function to retrieve data from the file
             dictionary = pickle.load(f)
         return dictionary
     except FileNotFoundError:
@@ -99,6 +121,9 @@ def retrieveFromFile():
         retrieveFromFile()
 
 
+# This function allows the user to delete any save files
+# Future additions:
+#   1. Since there may be .txt files present that are not save files, the program should prevent the user from deleting those
 def deleteSaveFile():
     print('List of account save files:')
     files = [f for f in os.listdir('.') if os.path.isfile(f)]
@@ -108,17 +133,21 @@ def deleteSaveFile():
         else:
             continue
     filechoice = input('\nWhich save file do you want to delete? ')
+    # Checks to see if the file is a .txt file (since only .txt files are present in the files list)
     if filechoice in files:
         os.remove(filechoice)
         print('Save file deleted!')
     else:
         print('That file does not exist')
 
+# Required variables to run the program
+active = True
+accountDictionary = {}
 
+# The start of the main program
 print('Welcome to the Account Creation Portal!')
+# A while loop to control the entire program flow and menu
 while active:
-    # while loop is for the menu
-    # create functions for each option
     print('\nWhat would you like to do?')
     print('\t1. Create a new account')
     print('\t2. View a list of existing accounts')
@@ -127,8 +156,9 @@ while active:
     print('\t5. Retrieve accounts from a specific file')
     print('\t6. Delete an account save file')
     print('\t7. Quit')
+    
+    # Based on user input, call the appropriate function
     choice = input()
-
     if choice == '1':
         createAccount()
     elif choice == '2':
@@ -138,6 +168,12 @@ while active:
     elif choice == '4':
         writeToFile(accountDictionary)
     elif choice == '5':
+        # The logic behind the next few lines is as follows:
+        # In a scenario where the user has created an account(s), and they desire to retrieve accounts from a save file,
+        # the program would have erased all created accounts and replaced the existing dictionary with the dictionary returned
+        # by the retrieveFromFile() dictictionary. In order to prevent this, I created a tempDictionary to place the dictionary
+        # returned by the function, and then update the current dictionary with the dictionary from the save file. In other
+        # words, I basically merged the 2 dictionaries
         tempDictionary = {}
         tempDictionary = retrieveFromFile()
         accountDictionary.update(tempDictionary)
