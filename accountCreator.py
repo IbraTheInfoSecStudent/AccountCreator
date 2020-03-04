b9e5db014ee216a06bdf69ce1aaf5a97cfcb2d8c
IbraTheInfoSecStudent <60770005+IbraTheInfoSecStudent@users.noreply.github.com>
2020-03-04 18:33:59 -0500

    updates
    added:
    1. saving user data to a file
    2. retrieving user data from a file
    3. exporting user data to an excel file

--- accountCreator.py
+++ accountCreator.py
@@ -3,7 +3,7 @@
 # Accounts from files can be retrieved. Save files can be deleted. Account details can be viewed.
 
 # Required modules
-import pickle, os, glob
+import pickle, os, glob, openpyxl
 
 
 # Creating a class Account. Each account the user creates is an object of this class
@@ -27,6 +27,14 @@ class UserInfo:
     def getUserInfo(self):
         print(f"Favourite Language: {self.favProg.title()}\nCar Owned: {self.carOwned}\nFavourite Food: {self.favFood}")
 
+    def getFavProg(self):
+        return self.favProg
+
+    def getCar(self):
+        return self.carOwned
+
+    def getFavFood(self):
+        return self.favFood
 
 # This function creates accounts. Requires user to input their name, desired username, and desired password
 def createAccount():
@@ -162,7 +170,6 @@ def addUserInfo():
         print('\t' + keys)
     user = input('\nEnter account name: ')
     if user in accountDictionary.keys():
-        # Loops through the entire dictionary, but only prints data once it finds the username the user chose
         favProg = input('Favourite programming language: ')
         car = input('Car owned: ')
         favFood = input('Favourite food: ')
@@ -179,18 +186,88 @@ def viewUserData():
         print('\t' + keys)
     user = input('\nEnter account name: ')
     if user in userInfoDictionary:
-        for value in userInfoDictionary.values():
-            value.getUserInfo()
+        for key, value in userInfoDictionary.items():
+            if user == key:
+                value.getUserInfo()
+
+
+def saveUserInfo(dictionary):
+    print('Current account save files: ')
+    # Prints all files in the current directory that contain a .txt extension
+    files = [f for f in os.listdir('.') if '.txt' in f]
+    for f in files:
+        print(f)
+
+    filechoice = input('\nWhich file should the accounts be saved to?\nIt can be one of the above files, or a new one.'
+                       '\nCAUTION: Existing files will be overwritten.\tChoose file: ')
+    # It does not matter if the user includes the .txt in what they input
+    if '.txt' in filechoice:
+        filename = filechoice
+    else:
+        filename = (filechoice + '.txt')
+
+    with open(filename, 'wb') as f:
+        # Uses pickle's dump() function to put all accounts from the dictionary into the save file
+        # If save file does not exist, it creates a new file automatically
+        pickle.dump(dictionary, f)
+    print('Saved to a file')
+
+
+def retrieveUserDataFromFile():
+    print('List of data save files:')
+    files = [f for f in os.listdir('.') if '.txt' in f]
+    for f in files:
+        print(f)
+
+    filechoice = input('\nWhich file do you want to retrieve accounts from? ')
+    if '.txt' in filechoice:
+        filename = filechoice
+    else:
+        filename = (filechoice + '.txt')
+
+    # Try and except block in order to catch the FileNotFoundError
+    try:
+        with open(filename, 'rb') as f:
+            # Uses pickle's load() function to retrieve data from the file
+            dictionary = pickle.load(f)
+        return dictionary
+    except FileNotFoundError:
+        print('That file does not exist\n')
+        retrieveFromFile()
+
+
+def exportToExcel():
+    name = input('Choose a file name: ')
+    if '.xlsx' not in name:
+        name = name+'.xlsx'
+    excel = openpyxl.Workbook()
+    sheet = excel['Sheet']
+    sheet.title = 'User data'
+    sheet = excel['User data']
+    sheet['A1'] = 'Username'
+    sheet['B1'] = 'Fav prog lang'
+    sheet['C1'] = 'Car owned'
+    sheet['D1'] = 'Fav food'
+    c = 2
+    for key, value in userInfoDictionary.items():
+        sheet['A' + str(c)] = key
+        sheet['B' + str(c)] = value.getFavProg()
+        sheet['C' + str(c)] = value.getCar()
+        sheet['D' + str(c)] = value.getFavFood()
+        c += 1
+    excel.save(name)
+    print('Exporting to excel complete: ' + name)
+
 
 # Dictionary where accounts are saved
 accountDictionary = {}
 userInfoDictionary = {}
 
 # The start of the main program
-print('Welcome to the Account Creation Portal!')
+print('Welcome to the Account & Data Management Portal!')
 # A while loop to control the entire program flow and menu
 while True:
-    print('\nWhat would you like to do?')
+    print('\n*** Account Management ***')
     print('\t1. Create a new account')
     print('\t2. View a list of existing accounts')
     print('\t3. View account information for a specific account')
@@ -198,12 +275,14 @@ while True:
     print('\t5. Retrieve accounts from a specific file')
     print('\t6. Delete an account save file')
     print('\t7. View save file sizes')
-    print('\t8. Quit')
-    print('*** Data Actions ***')
+    print('\t8. Quit\n')
+    print('*** Data Management ***')
     print('\t9. Add user data')
     print('\t10. View user data')
-    print('\t11. View statistics report')
-    print('\t12. Export statistics as a text file')
+    print('\t11. Save user data to a file')
+    print('\t12. Retrieve user data from a file')
+    print('\t13. Export data to excel file (.xlsx)')
+    print('\t14. Export statistics as a text file')
 
     # Based on user input, call the appropriate function
     choice = input()
@@ -237,5 +316,13 @@ while True:
         addUserInfo()
     elif choice == '10':
         viewUserData()
+    elif choice == '11':
+        saveUserInfo(userInfoDictionary)
+    elif choice == '12':
+        temp1Dictionary = {}
+        temp1Dictionary = retrieveUserDataFromFile()
+        userInfoDictionary.update(temp1Dictionary)
+    elif choice == '13':
+        exportToExcel()
     else:
         print('That option does not exist. Try again')
